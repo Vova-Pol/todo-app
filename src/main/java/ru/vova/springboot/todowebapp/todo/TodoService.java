65b5c7f5267a915c34e208e08af163ec8170c86d
Vova Pol <ya-chai777@yandex.ru>
2023-09-07 15:20:20 +0300

    Написал базовую логику фичи добавления todo

--- src/main/java/ru/vova/springboot/todowebapp/todo/TodoService.java
+++ src/main/java/ru/vova/springboot/todowebapp/todo/TodoService.java
@@ -9,17 +9,22 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
+    private static int todosCount = 0;
 
     static {
-        todos.add(new Todo(1, "Vova", "Learn Spring",
+        todos.add(new Todo(++todosCount, "Vova", "Learn Spring",
                 LocalDate.now().plusMonths(2), false));
-        todos.add(new Todo(2, "Vova", "Learn Php",
+        todos.add(new Todo(++todosCount, "Vova", "Learn Php",
                 LocalDate.now().plusMonths(4), false));
-        todos.add(new Todo(3, "Vova", "Create FullStack App",
+        todos.add(new Todo(++todosCount, "Vova", "Create FullStack App",
                 LocalDate.now().plusMonths(1), false));
     }
 
     public List<Todo> getTodos() {
         return todos;
+    }
+
+    public void addTodo(String userName, String description, LocalDate targetDate, boolean isDone) {
+        todos.add(new Todo(++todosCount, userName, description, targetDate, isDone));
     }
 }
