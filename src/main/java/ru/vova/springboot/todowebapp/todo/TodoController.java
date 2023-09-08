92e7fd0f2d2cffc14ebfa01a00098cccbf7387f6
Vova Pol <ya-chai777@yandex.ru>
2023-09-08 14:43:11 +0300

    Сделал валидацию поля формы

--- src/main/java/ru/vova/springboot/todowebapp/todo/TodoController.java
+++ src/main/java/ru/vova/springboot/todowebapp/todo/TodoController.java
@@ -1,11 +1,12 @@
 package ru.vova.springboot.todowebapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.time.LocalDate;
@@ -26,15 +27,22 @@ public class TodoController {
     }
 
     @RequestMapping(value ="add-todo", method = RequestMethod.GET)
-    public String addTodoPage() {
+    public String addTodoPage(ModelMap model) {
+        String userName = (String) model.get("name");
+        Todo todo = new Todo(0, userName, "", LocalDate.now(), false);
+        model.put("todo", todo);
         return "todo";
     }
 
     @RequestMapping(value ="add-todo", method = RequestMethod.POST)
-    public String addTodo(ModelMap model, @RequestParam String description) {
+    public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "todo";
+        }
 
         String userName = (String) model.get("name");
-        todoService.addTodo(userName, description, LocalDate.now(), false);
+        todoService.addTodo(userName, todo.getDescription(), LocalDate.now(), false);
         return "redirect:todos-list";
     }
 }
